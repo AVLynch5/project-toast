f50c6a1495da3ac16f6b7d700a36c3425e5aa3b3
alynch <anthonyvlynch5@gmail.com>
2024-11-01 14:57:27 -0400

    exercise5 - modify ToastShelf ol tag to add aria attributes for accessibility

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -5,9 +5,14 @@ import { ToastContext } from "../ToastProvider";
 import styles from "./ToastShelf.module.css";
 
 function ToastShelf() {
-  const {toastArray} = React.useContext(ToastContext);
+  const { toastArray } = React.useContext(ToastContext);
   return (
-    <ol className={styles.wrapper}>
+    <ol
+      className={styles.wrapper}
+      role="region"
+      aria-live="polite"
+      aria-label="Notification"
+    >
       {toastArray.map(({ message, variantType, id }) => {
         return (
           <li className={styles.toastWrapper} key={id}>
